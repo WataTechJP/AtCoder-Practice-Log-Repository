83a6b6c28bbbf521752c6e2743483bd9f78bd952
WataTechJP <wattech3371@gmail.com>
2026-02-09 02:38:32 -0800

    ABC311 B [WA]

---
+++ ABC/ABC311/B/B.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# https://atcoder.jp/contests/abc311/tasks/abc311_b
+import sys
+
+def main():
+    input = sys.stdin.readline
+    ## Answer here
+    N, D = map(int, input().split())
+    S = [input().strip() for _ in range(N)]
+    
+
+if __name__ == "__main__":
+    main()
